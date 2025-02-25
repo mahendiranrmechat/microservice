668ab419ca3dcfa2c356ee294b8584fe0846de7b
mahendiranrmechat <130583491+mahendiranrmechat@users.noreply.github.com>
2025-02-25 17:33:16 +0530

    Update AuthController.java

--- login-demo-master/src/main/java/loginMahe/logindemo/controller/AuthController.java
+++ login-demo-master/src/main/java/loginMahe/logindemo/controller/AuthController.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
     LoginService loginService;
 
 
-    @GetMapping
+    @GetMapping("login")
     public String loginMethod(){
          
          return loginService.loginMethod();
